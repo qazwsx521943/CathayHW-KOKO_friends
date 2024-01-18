53a2e7cf2337229b99644d7379461f16168fa78f
Jason's Macbook <qazwsx5219430630@gmail.com>
2024-01-18 11:14:38 +0800

    Initial Commit

---
+++ KOKO_friends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KOKO_friends
+//
+//  Created by 鍾哲玄 on 2024/1/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
