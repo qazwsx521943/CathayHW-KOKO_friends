db871a183424acde9f7412e24cccf1b401f74af6
Jason's Macbook <qazwsx5219430630@gmail.com>
2024-01-22 02:21:20 +0800

    [feat] Add KokoFriendListViewController
    - Add KokoFriendListViewModel
    - Conditional rendering depending on viewModel data and implemented using container view in KokoFriendListViewController

--- KOKO_friends/Extensions/UIView+EXT.swift
+++ KOKO_friends/Extensions/UIView+EXT.swift
@@ -16,4 +16,19 @@ extension UIView {
 			view.bottomAnchor.constraint(equalTo: bottomAnchor)
 		])
 	}
+
+	func shadowEffect(cornerRadius: CGFloat) {
+		let size = bounds.size
+		let width = size.width
+		let height = size.height
+
+		let ovalRect = CGRect(x: 5, y: height + 5, width: width - 10, height: 15)
+
+		layer.shadowPath = UIBezierPath(roundedRect: ovalRect, cornerRadius: cornerRadius).cgPath
+//		layer.masksToBounds = false
+		layer.shadowRadius = 10
+		layer.shadowColor = UIColor.gray.cgColor
+		layer.shadowOpacity = 0.5
+		layer.shadowOffset = CGSize(width: 10, height: 10)
+	}
 }
--- KOKO_friends/Views/KokoFriendListViewModel.swift
+++ KOKO_friends/Views/KokoFriendListViewModel.swift
@@ -9,7 +9,15 @@ import Foundation
 import Combine
 
 class KokoFriendListViewModel {
+	@Published var friendResponseType: APIRequest = .noFriends
+
 	@Published var currentUser: KKUser?
+
+	@Published var friendList: [KKFriend] = []
+
+	@Published var searchedList: [KKFriend] = []
+
+	@Published var pendingInvitation: [KKFriend] = []
 
 	private let kokoService: KokoServiceProtocol
 
@@ -33,5 +41,39 @@ class KokoFriendListViewModel {
 				self.currentUser = user
 			}
 			.store(in: &anyCancellables)
+	}
+
+	public func fetchFriends(type: Int) {
+		kokoService.getUserFriends(type: type)
+			.receive(on: RunLoop.main)
+			.sink { completion in
+				switch completion {
+				case .finished:
+					print("friends fetched")
+				case .failure(let error):
+					print("error: \(error.localizedDescription)")
+				}
+			} receiveValue: { friends in
+				self.friendList = friends.filter { $0.status != .pending }
+				self.pendingInvitation = friends.filter { $0.status == .pending }
+			}
+			.store(in: &anyCancellables)
+	}
+
+	public func searchFriend(keyword: String) {
+		searchedList = friendList.filter { $0.name.contains(keyword) }
+	}
+
+	public func acceptFriendRequest(from friend: KKFriend) {
+		if let idx = pendingInvitation.firstIndex(of: friend) {
+			let acceptedFriend = pendingInvitation.remove(at: idx)
+			friendList.append(acceptedFriend)
+		}
+	}
+
+	public func rejectFriendRequest(from friend: KKFriend) {
+		if let idx = pendingInvitation.firstIndex(of: friend) {
+			pendingInvitation.remove(at: idx)
+		}
 	}
 }
