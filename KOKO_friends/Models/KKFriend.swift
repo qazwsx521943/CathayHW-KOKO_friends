8b14ea1eea90fa38a87162c2201aeba125fbb01a
Jason's Macbook <qazwsx5219430630@gmail.com>
2024-01-21 23:38:26 +0800

    [feat] Add FriendListTableVIewCell
    - Status toggle done by dynamically setting constraints

--- KOKO_friends/Models/KKFriend.swift
+++ KOKO_friends/Models/KKFriend.swift
@@ -10,8 +10,15 @@ import Foundation
 // curl https://dimanyen.github.io/friend1.json
 struct KKFriend: Codable {
 	let name: String
-	let status: Int
+	let status: FriendStatus
 	let isTop: String
 	let fid: String
 	let updateDate: String
+}
+
+// https://app.zeplin.io/project/5c498614493bf5bf68258c5a/screen/5d77637750db6d7380db8c87
+enum FriendStatus: Int, Codable {
+	case pending = 0
+	case friend
+	case inviting
 }
